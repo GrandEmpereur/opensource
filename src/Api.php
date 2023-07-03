5d76bb0164d1154301e8b5a2b4404937921e3197
Empereur <bartosikpatrick2@gmail.com>
2023-07-03 16:04:23 +0100

    📦 ajouter(ProjectSettings.json, VSWorkspaceState.json, .wsuo, slnx.sqlite) : ajouter des fichiers de configuration pour le projet 📦 ajouter(composer.json) : ajouter des dépendances pour le projet (google/apiclient, symfony/http-client) 🔧 corriger(index.php) : modifier la logique pour récupérer et afficher les événements 🔧 corriger(Api.php) : modifier la logique pour récupérer les événements à partir de l'API Google Calendar 🔧 corriger(ApiTest.php) : supprimer les tests inutiles Les fichiers de configuration ajoutés sont nécessaires pour le bon fonctionnement du projet. Les dépendances ajoutées dans composer.json permettent d'utiliser les bibliothèques google/apiclient et symfony/http-client. Les modifications apportées à index.php et Api.php permettent de récupérer les événements à partir de l'API Google Calendar et de les afficher. Les tests inutiles ont été supprimés de ApiTest.php.

--- src/Api.php
+++ src/Api.php
@@ -4,20 +4,55 @@ declare(strict_types=1);
 
 namespace Empereur\Opensource;
 
-class Api
-{
-    public function getRandomNumber(): int
-    {
-        return rand(0, 100);
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Google_Client;
+use Google_Service_Calendar;
+use Google_Service_Calendar_Event;
+use function \json_encode;
+
+class Api {
+    private $client;
+    private $httpClient;
+
+    public function __construct() {
+        $this->client = new Google_Client();
+        $this->client->setApplicationName('Calendar x RATP');
+        //$this->client->setClientId('536453816458-ajmiqhr1uc137pn8q3159i12djvnoe51.apps.googleusercontent.com');
+        //$this->client->setClientSecret('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');
+        $this->client->setDeveloperKey('AIzaSyDfkCR7zb1Vy55fB7uP8OpKA151rs1Urjo');
+        // $this->client->setAccessToken('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');
+        $this->client->setScopes(Google_Service_Calendar::CALENDAR);
+        $this->client->setHttpClient(new \GuzzleHttp\Client(['verify' => false]));
     }
 
-    public function getDate(): \DateTime
-    {
-        return new \DateTime();
-    }
+    public function getEvents(): array|object {
+        $service = new Google_Service_Calendar($this->client);
+        $calendarId = 'bartosikpatrick1@gmail.com';
+        $currentDate = date('Y-m-d');
+    
+        $optParams = array(
+            'orderBy' => 'startTime',
+            'singleEvents' => true,
+            'timeMin' => $currentDate . 'T00:00:00Z',
+            'timeMax' => date('Y-m-d', strtotime($currentDate . ' + 1 day')) . 'T00:00:00Z',
+        );
+    
+        $results = $service->events->listEvents($calendarId, $optParams);
+        $events = $results->getItems();
 
-    public function getDateAsString(): string
-    {
-        return $this->getDate()->format('Y-m-d H:i:s');
+        $formattedEvents = [];
+        foreach ($events as $event) {
+            $formattedEvent = [
+                'name' => $event->getSummary(),
+                'attendees' => $event->getAttendees(),
+                'location' => $event->getLocation(),
+                'start' => $event->getStart()->getDateTime(),
+                'end' => $event->getEnd()->getDateTime(),
+            ];
+            $formattedEvents[] = $formattedEvent;
+        }
+
+        return $formattedEvents;
     }
 }
--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -5,17 +5,4 @@ use PHPUnit\Framework\TestCase;
 
 class ApiTest extends TestCase
 {
-    public function testGetRandomNumber()
-    {
-        $api = new Api();
-        $this->assertIsInt($api->getRandomNumber());
-    }
-
-    public function testGetDate()
-    {
-        $api = new Api();
-        $this->assertInstanceOf(\DateTime::class, $api->getDate());
-        $this->assertIsString($api->getDateAsString());
-        $this->assertSame(date('Y-m-d H:i:s'), $api->getDateAsString());
-    }
 }
