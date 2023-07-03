143e7b095d9d1836e0ff0511300993ecf5cad6cb
Empereur <bartosikpatrick2@gmail.com>
2023-07-03 23:03:16 +0100

    🔧 corriger(server.ts) : changer la casse de la variable de port de minuscules à majuscules (PORT) ✨ fonctionnalité(server.ts) : ajouter la prise en charge de la variable d'environnement process.env.PORT La variable de port est maintenant nommée PORT, ce qui améliore la cohérence avec les conventions de nommage car PORT est une constante. La prise en charge d'une variable d'environnement permet à l'application d'être plus flexible car elle peut maintenant s'exécuter sur n'importe quel port disponible spécifié via la variable d'environnement process.env.PORT.
    📝 docs(.env, .env.example) : ajouter les fichiers .env et .env.example
    Les fichiers .env et .env.example ont été ajoutés pour gérer les variables d'environnement de l'application. Cela permet de stocker des informations sensibles telles que les clés d'API et les identifiants de calendrier de manière sécurisée.
    
    🔧 corriger(composer.json) : ajouter la dépendance vlucas/phpdotenv
    La dépendance vlucas/phpdotenv a été ajoutée au fichier composer.json pour permettre la gestion des variables d'environnement à partir du fichier .env. Cela facilite la configuration de l'application en utilisant des variables d'environnement plutôt que des valeurs codées en dur.
    
    🔧 corriger(Api.php) : charger les variables d'environnement à partir du fichier .env
    Les variables d'environnement sont maintenant chargées à partir du fichier .env en utilisant la bibliothèque Dotenv. Cela permet de sécuriser les informations sensibles telles que le nom de l'application, la clé du développeur et l'identifiant du calendrier en les stockant dans un fichier séparé plutôt que de les inclure directement dans le code source.

--- src/Api.php
+++ src/Api.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Google_Client;
 use Google_Service_Calendar;
 use Google_Service_Calendar_Event;
+use Dotenv\Dotenv;
 use function \json_encode;
 
 class Api {
@@ -16,26 +17,26 @@ class Api {
     private $httpClient;
 
     public function __construct() {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+        $dotenv->load();
+
         $this->client = new Google_Client();
-        $this->client->setApplicationName('Calendar x RATP');
-        //$this->client->setClientId('536453816458-ajmiqhr1uc137pn8q3159i12djvnoe51.apps.googleusercontent.com');
-        //$this->client->setClientSecret('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');
-        $this->client->setDeveloperKey('AIzaSyDfkCR7zb1Vy55fB7uP8OpKA151rs1Urjo');
-        // $this->client->setAccessToken('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');
+        $this->client->setApplicationName($_ENV['APPLICATION_NAME']);
+        $this->client->setDeveloperKey($_ENV['DEVELOPER_KEY']);
         $this->client->setScopes(Google_Service_Calendar::CALENDAR);
         $this->client->setHttpClient(new \GuzzleHttp\Client(['verify' => false]));
     }
 
     public function getEvents(): array|object {
         $service = new Google_Service_Calendar($this->client);
-        $calendarId = 'bartosikpatrick1@gmail.com';
+        $calendarId = $_ENV['CALENDAR_ID'];
         $currentDate = date('Y-m-d');
     
         $optParams = array(
             'orderBy' => 'startTime',
             'singleEvents' => true,
             'timeMin' => $currentDate . 'T00:00:00Z',
-            'timeMax' => date('Y-m-d', strtotime($currentDate . ' + 1 day')) . 'T00:00:00Z',
+            'timeMax' => date('Y-m-d', strtotime($currentDate . ' + 7 days')) . 'T00:00:00Z',
         );
     
         $results = $service->events->listEvents($calendarId, $optParams);
